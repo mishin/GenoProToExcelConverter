13b2871d0d5e3dc8a90e4415e32de88518a6349b
Nikolay Mishin <mi@ya.ru>
2017-02-21 13:21:58 +0300

    add Model Pedigree object

---
+++ src/main/java/Padigree.java
@@ -0,0 +1,254 @@
+/**
+ * Class for storing Padigree.
+ */
+class Padigree {
+    private String ID;
+    private String title;
+    private String prefix;
+    private String firstName;
+    private String midName;
+    private String lastName;
+    private String suffix;
+    private String nickname;
+    private String fatherId;
+    private String motherId;
+    private String email;
+    private String webpage;
+    private String dateOfBirth;
+    private String dateOfDeath;
+    private String gender;
+    private String isLiving;
+    private String placeOfBirth;
+    private String placeOfDdeath;
+    private String cemetery;
+    private String schools;
+    private String jobs;
+    private String workPlaces;
+    private String placesOfLiving;
+    private String general;
+
+// --Commented out by Inspection START (21.02.2017 13:01):
+//    public Padigree(String ID, String title, String prefix, String firstName, String midName, String lastName, String suffix, String nickname, String fatherId, String motherId, String email, String webpage, String dateOfBirth, String dateOfDeath, String gender, String isLiving, String placeOfBirth, String placeOfDdeath, String cemetery, String schools, String jobs, String workPlaces, String placesOfLiving, String general) {
+//        this.ID = ID;
+//        this.title = title;
+//        this.prefix = prefix;
+//        this.firstName = firstName;
+//        this.midName = midName;
+//        this.lastName = lastName;
+//        this.suffix = suffix;
+//        this.nickname = nickname;
+//        this.fatherId = fatherId;
+//        this.motherId = motherId;
+//        this.email = email;
+//        this.webpage = webpage;
+//        this.dateOfBirth = dateOfBirth;
+//        this.dateOfDeath = dateOfDeath;
+//        this.gender = gender;
+//        this.isLiving = isLiving;
+//        this.placeOfBirth = placeOfBirth;
+//        this.placeOfDdeath = placeOfDdeath;
+//        this.cemetery = cemetery;
+//        this.schools = schools;
+//        this.jobs = jobs;
+//        this.workPlaces = workPlaces;
+//        this.placesOfLiving = placesOfLiving;
+//        this.general = general;
+//    }
+// --Commented out by Inspection STOP (21.02.2017 13:01)
+
+    public Padigree() {
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getFatherId() {
+        return fatherId;
+    }
+
+    public void setFatherId(String fatherId) {
+        this.fatherId = fatherId;
+    }
+
+    public String getMotherId() {
+        return motherId;
+    }
+
+    public void setMotherId(String motherId) {
+        this.motherId = motherId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebpage() {
+        return webpage;
+    }
+
+    public void setWebpage(String webpage) {
+        this.webpage = webpage;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(String dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getIsLiving() {
+        return isLiving;
+    }
+
+    public void setIsLiving(String isLiving) {
+        this.isLiving = isLiving;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public String getPlaceOfDdeath() {
+        return placeOfDdeath;
+    }
+
+    public void setPlaceOfDdeath(String placeOfDdeath) {
+        this.placeOfDdeath = placeOfDdeath;
+    }
+
+    public String getCemetery() {
+        return cemetery;
+    }
+
+    public void setCemetery(String cemetery) {
+        this.cemetery = cemetery;
+    }
+
+    public String getSchools() {
+        return schools;
+    }
+
+    public void setSchools(String schools) {
+        this.schools = schools;
+    }
+
+    public String getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(String jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getWorkPlaces() {
+        return workPlaces;
+    }
+
+    public void setWorkPlaces(String workPlaces) {
+        this.workPlaces = workPlaces;
+    }
+
+    public String getPlacesOfLiving() {
+        return placesOfLiving;
+    }
+
+    public void setPlacesOfLiving(String placesOfLiving) {
+        this.placesOfLiving = placesOfLiving;
+    }
+
+    public String getGeneral() {
+        return general;
+    }
+
+    public void setGeneral(String general) {
+        this.general = general;
+    }
+}
